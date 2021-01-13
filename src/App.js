e00783fe6a0c87b7b77d96753c328efcb6058446
cipollafrancesco <francescocpll@gmail.com>
2021-01-13 13:54:07 +0100

    Fixed material-ui classNames collisions between Applications

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {StylesProvider} from '@material-ui/core'
+import {createGenerateClassName, StylesProvider} from '@material-ui/core'
 
 import Pricing from './components/Pricing'
 import Landing from './components/Landing'
 
+// Aggiunge un prefisso alle classi CSS-in-JS in prod (avoid classNames collision)
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'marketing'
+})
+
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing}/>
