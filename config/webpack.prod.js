b51b60f82aa4499e94c283b33a4b39fbbd2a6b0e
cipollafrancesco <francescocpll@gmail.com>
2021-01-13 11:54:43 +0100

    Added marketing deployment

---
+++ config/webpack.prod.js
@@ -0,0 +1,25 @@
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const {merge} = require('webpack-merge')
+
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/marketing/latest/' // base path in src fetch
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'marketing',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './MarketingApp': './src/bootstrap'
+            },
+            shared: packageJson.dependencies,
+        }),
+    ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
