692e85de1636f417619b76408ceb701dc05e4e3d
cipollafrancesco <francescocpll@gmail.com>
2021-01-14 18:10:37 +0100

    Added Memory Router

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -8,13 +8,16 @@ const packageJson = require('../package.json')
 const {merge} = require('webpack-merge')
 const commonConfig = require('./webpack.common')
 
+const SERVER_PORT = 8081
+
 const devConfig = {
     mode: 'development',
     devServer: {
-        port: 8081,
-        historyApiFallback: {
-            index: 'index.html',
-        }
+        port: SERVER_PORT,
+        historyApiFallback: true // with object doesn't work
+    },
+    output: {
+        publicPath: `http://localhost:${SERVER_PORT}/`
     },
     plugins: [
         new HtmlWebpackPlugin({
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,21 +1,38 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
+import {createBrowserHistory, createMemoryHistory} from 'history'
 
-console.log('Marketing works!')
+console.info('>>> Marketing works!')
 
 // Mount func to start up the application
-const mount = el => {
-    ReactDOM.render(<App/>, el)
-}
+const mount = (el, {onNavigate, defaultHistory, initialPath}) => {
 
-export {mount}
+    // Create MEMORY HISTORY that all the children must have
+    const history = defaultHistory || createMemoryHistory({initialEntries: [initialPath]})
+
+    onNavigate &&
+    history.listen(onNavigate) // Navigation listener
+
+    ReactDOM.render(<App history={history}/>, el)
+
+    return ({
+        onParentNavigate: ({pathname: nextPathname}) => {
+            // console.log('[Container] UPDATING PATHNAME', nextPathname)
+            history.location.pathname !== nextPathname &&
+            history.push(nextPathname)
+        }
+    })
+}
 
 // If we are in dev and in isolation call mount immediately
 if (process.env.NODE_ENV === 'development') {
     // root element to attach application
     const devRoot = document.querySelector('#_marketing-dev-root')
 
-    // If present calls mount fuctions
-    devRoot && mount(devRoot)
+    // If present calls mount functions
+    devRoot && mount(devRoot, {defaultHistory: createBrowserHistory()}) // In DEV I'll use BrowserHistory
 }
+
+// EXPORTING MOUNT FUNCTION
+export {mount}
